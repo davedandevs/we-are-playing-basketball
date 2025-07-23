76f0b8ffaa77c5c4191ff601710026c5aa440e2c
Daniil Borisevich <dborisevich@whoosh.bike>
2025-07-23 16:57:29 +0300

    init 2

---
+++ src/main/java/by/vibe/weplayingbasketball/controller/StartController.java
@@ -0,0 +1,16 @@
+package by.vibe.weplayingbasketball.controller;
+
+
+import by.vibe.weplayingbasketball.model.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StartController {
+
+  @GetMapping("/start")
+  public ResponseEntity<?> start() {
+    return ResponseEntity.ok(SuccessResponse.builder().success(true).build());
+  }
+}
---
+++ src/main/java/by/vibe/weplayingbasketball/model/SuccessResponse.java
@@ -0,0 +1,14 @@
+package by.vibe.weplayingbasketball.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SuccessResponse {
+  private boolean success;
+}
