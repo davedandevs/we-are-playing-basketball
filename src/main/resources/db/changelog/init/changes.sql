28e225d26a50c44117ad17ca9464223feefe039c
Daniil Borisevich <borisevich.daniel@gmail.com>
2025-08-04 08:55:43 +0300

    [TECH TASK]: Small fixes

--- src/main/resources/db/changelog/init/changes.sql
+++ src/main/resources/db/changelog/init/changes.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id         SERIAL PRIMARY KEY,
-    username      VARCHAR(255)  NOT NULL UNIQUE,
+    username      VARCHAR(50)  NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255),
-    last_name  VARCHAR(255),
-    role       VARCHAR(255)  NOT NULL CHECK (role IN ('ADMIN', 'USER'))
+    first_name VARCHAR(100),
+    last_name  VARCHAR(100),
+    role       VARCHAR(30)  NOT NULL CHECK (role IN ('admin', 'user'))
     );
 
 CREATE TABLE IF NOT EXISTS seasons
 (
     id         SERIAL PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL UNIQUE,
+    name       VARCHAR(50) NOT NULL UNIQUE,
     start_date DATE        NOT NULL,
     end_date   DATE        NOT NULL
     );
@@ -19,16 +19,16 @@ CREATE TABLE IF NOT EXISTS seasons
 CREATE TABLE IF NOT EXISTS teams
 (
     id   SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
+    name VARCHAR(100) NOT NULL UNIQUE
     );
 
 CREATE TABLE IF NOT EXISTS players
 (
     id         SERIAL PRIMARY KEY,
     team_id    INT REFERENCES teams (id),
-    first_name VARCHAR(255) NOT NULL,
-    last_name  VARCHAR(255) NOT NULL,
-    position   VARCHAR(255),
+    first_name VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) NOT NULL,
+    position   VARCHAR(30),
     age        INT,
     height     INT,
     weight     INT
